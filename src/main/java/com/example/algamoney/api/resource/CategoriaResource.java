9a28243712658ff1e86f5884913dc61ae9198d04
fcsouza <fcsouza18@hotmail.com>
2020-01-08 15:40:22 -0300

    Ajustes no recurso categorias

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,7 +3,6 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +18,8 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository; 
 	
 	@GetMapping
-	public ResponseEntity<?> listar(){
-		List<Categoria> categorias = categoriaRepository.findAll();
-		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();
 	}
 
 }
